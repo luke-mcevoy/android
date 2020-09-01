187e1357b018225efdd98b34402b0da111ee534c
lmcevoy <lmcevoy@stevens.edu>
2020-08-31 20:18:46 -0400

    assignment1 android app intitial commit

---
+++ assignment1/application/app/src/main/java/cs522/stevens/assignment1/mcevoy/MainActivity.java
@@ -0,0 +1,14 @@
+package cs522.stevens.assignment1.mcevoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
