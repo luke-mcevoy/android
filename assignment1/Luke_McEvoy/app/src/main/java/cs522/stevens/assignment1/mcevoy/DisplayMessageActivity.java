2a036f3c97cbefa00f3f65d79589609a06c1609d
lmcevoy <lmcevoy@stevens.edu>
2020-09-01 09:17:10 -0400

    works. making default textview text hidden

--- assignment1/Luke_McEvoy/app/src/main/java/cs522/stevens/assignment1/mcevoy/DisplayMessageActivity.java
+++ assignment1/Luke_McEvoy/app/src/main/java/cs522/stevens/assignment1/mcevoy/DisplayMessageActivity.java
@@ -2,7 +2,9 @@ package cs522.stevens.assignment1.mcevoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
--- assignment1/Luke_McEvoy/app/src/main/java/cs522/stevens/assignment1/mcevoy/MainActivity.java
+++ assignment1/Luke_McEvoy/app/src/main/java/cs522/stevens/assignment1/mcevoy/MainActivity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
