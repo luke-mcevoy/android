d9b51359943ae761ce1924518db5b5f83061f352
lmcevoy <lmcevoy@stevens.edu>
2020-09-01 09:06:57 -0400

    built message intent + dispaly message activity

---
+++ assignment1/Luke_McEvoy/app/src/main/java/cs522/stevens/assignment1/mcevoy/DisplayMessageActivity.java
@@ -0,0 +1,14 @@
+package cs522.stevens.assignment1.mcevoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
--- assignment1/Luke_McEvoy/app/src/main/java/cs522/stevens/assignment1/mcevoy/MainActivity.java
+++ assignment1/Luke_McEvoy/app/src/main/java/cs522/stevens/assignment1/mcevoy/MainActivity.java
@@ -1,8 +1,12 @@
 package cs522.stevens.assignment1.mcevoy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
